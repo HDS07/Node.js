7470f802fd518c3cf59d807265faf51894e19976
harshdeep <hs4205170@gmail.com>
2024-04-26 22:32:20 +0530

    upated file

--- hello.js
+++ hello.js
@@ -2,4 +2,6 @@ var http=require('http');
 http.createServer(function (req,res) {
     res.writeHead(200,{'content-type': 'text/html'});
     res.end('hello world');
+    console.log("1233");
 }).listen(1234);
+console.log("server is running at port")
